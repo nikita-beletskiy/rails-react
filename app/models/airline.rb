c78f11ebb9d85de816315d764faea08b76142a71
Nikita Beletskiy <bel.nikita3197@gmail.com>
2021-07-04 20:39:03 +0300

    Added some methods Airline model and seeds

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,10 @@
 class Airline < ApplicationRecord
   has_many :reviews
+  def slugify
+    self.slug = name.parameterize
+  end
+
+  def avg_score
+    reviews.average(:score).round(2).to_f
+  end
 end
